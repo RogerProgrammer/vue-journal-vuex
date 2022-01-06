1777c73c7646ec7ae014620bc6a3ceef4fb7902a
Roger Badillo <rogeralexanderbr@ufps.edu.co>
2022-01-06 05:41:51 -0500

    Primer commit

---
+++ src/modules/daybook/store/journal/state.js
@@ -0,0 +1,23 @@
+export default () => ({
+    isLoading: true,
+    entries: [
+        {
+            id: new Date().getTime(),
+            date: new Date().toDateString(),
+            text: 'Adipisicing est velit anim enim sunt occaecat. Minim elit labore tempor laboris dolore ullamco ex reprehenderit eu amet voluptate. Dolor non nulla nisi voluptate est magna sint tempor anim laborum cupidatat non. Tempor dolor pariatur tempor adipisicing in dolor est culpa commodo consectetur fugiat.',
+            picture: null
+        },
+        {
+            id: new Date().getTime()+1000,
+            date: new Date().toDateString(),
+            text: 'Culpa nisi magna velit laboris incididunt esse. Laboris ad excepteur anim ullamco adipisicing nisi est excepteur reprehenderit. Incididunt esse fugiat adipisicing et velit cupidatat tempor amet. Voluptate fugiat anim aute ea fugiat. Velit nulla deserunt incididunt magna nostrud proident reprehenderit sint quis laboris ut amet qui. Minim Lorem ea ea non pariatur aute tempor nisi id est exercitation dolore et.',
+            picture: null
+        },
+        {
+            id: new Date().getTime()+2000,
+            date: new Date().toDateString(),
+            text: 'Consectetur exercitation ex aliqua anim tempor exercitation mollit adipisicing dolor excepteur ipsum proident Lorem do. Occaecat nulla dolore elit adipisicing minim occaecat sint occaecat adipisicing sint esse irure laborum nisi. Cupidatat quis nulla pariatur ut quis sunt voluptate commodo voluptate aliqua.',
+            picture: null
+        }
+    ]
+})
